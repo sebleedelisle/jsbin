ce3ae5591f27b12a9f2052066ae857f5f1e675ee
Seb Lee-Delisle <sebleedelisle@gmail.com>
2012-03-24 10:49:12 +0000

    updating version number so live version works

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 // database settings
 define('DB_NAME', 'jsbin');
-define('DB_USER', 'root');  // Your MySQL username
-define('DB_PASSWORD', 'root'); // ...and password
+define('DB_USER', 'jsbin');  // Your MySQL username
+define('DB_PASSWORD', 'jo-say-b33'); // ...and password
 define('DB_HOST', 'localhost');  // 99% chance you won't need to change this value
 
 // change this to suite your offline detection
 define('OFFLINE', is_dir('/Users/'));
-
+#define('OFFLINE', true);
 define('HOST', 'http://localhost');
 
 // if you're running from a subdirectory, change this to the start of the
